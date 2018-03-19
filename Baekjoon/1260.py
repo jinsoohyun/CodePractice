0c5ffbb8cced7cc8bd82e2622d1e88951727e458
HorolLooloo <jsh05042@gmail.com>
2018-03-20 01:04:17 +0900

    2018_03_20

--- Baekjoon/1260.py
+++ Baekjoon/1260.py
@@ -1,44 +1,51 @@
 #-*-coding: utf-8-*-
-#graph = {}
+graph = {}
 
-'''
-N, M, V = raw_input().split(' ')
-for
-'''
+N, M, V = map(int,raw_input().split(' '))
 
-graph = {'1': set(['2', '3', '4']),
-         '2': set(['1', '4']),
-         '3': set(['1', '4']),
-         '4': set(['1', '3', '2'])
-         }
-'''
-def dfs(graph, root):
-  visited = [] # 각 꼭짓점(vertex)이 방문되었는지 기록
-  stack = [root, ]
- 
-  while stack: # stack 이 비게 되면 탐색 끝
-    vertex = stack.pop() # 방문되어지고 있는 꼭짓점
-    if vertex not in visited: #
+#generate dictionary
+for i in range(N): graph[str(i+1)] = []
+for i in range(M):
+    d1, d2 = map(str,raw_input().split(' '))
+    graph[d1].append(d2)
+    graph[d2].append(d1)
+
+
+def BFS(graph, root, N):
+  visited = [] # 방문한 곳을 기록
+  queue = [root] # 큐에 시작점을 줄세움
+
+  while queue: # queue 가 빌 때 까지 탐색을 계속
+    vertex = queue.pop(0) # 큐의 맨 앞의 원소를 방문할 꼭짓점으로 설정
+
+    if vertex not in visited: # 꼭짓점이 방문된 적이 없다면 방문 기록에 추가
       visited.append(vertex)
-      stack.extend(graph[vertex] - visited)
- 
-  return visited
+      if (len(visited) == N):
+          return visited
 
-
-'''
-
-def dfs_recursive(graph, root, visited=None):
-  if visited is None:
-    visited = set()
-
-  visited.add(root)
-
-  # 주어진 root 과 연결되어 있는 다른 node 들에 대해 방문하지 않은 것들만 재귀 호출합니다.
-  for node in graph[root] - visited:
-      dfs_recursive(graph, node, visited)
+      for node in graph[vertex]: # 꼭짓점에 연결된 노드들 중
+        if node not in visited: # 방문 안 된 곳 만을
+          queue.append(node) # 큐에 줄세움
 
   return visited
 
+def DFS(graph, root, N):
+    stack = []
+    visited = []
+
+    stack.extend(root)
+    while(stack):
+        outputFromStack = stack.pop()
+        visited.extend(outputFromStack)
+        if (len(visited) == N):
+          return visited
+
+        inputToStack = list(set(graph[outputFromStack]) - set(visited))
+        inputToStack.sort()
+        inputToStack = inputToStack[::-1]
+        stack.extend(inputToStack)
+    return visited
 
 
-print (dfs_recursive(graph, '1'))
+print ' '.join(DFS(graph, str(V), N))
+print ' '.join(BFS(graph, str(V), N))
